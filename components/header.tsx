f5fcdbd329d79f73a273fa35619c8e47c1d26681
Daniel Gamboa <danielgamboa10@gmail.com>
2025-03-07 17:23:34 -0600

    Remove comment from header

--- components/header.tsx
+++ components/header.tsx
@@ -13,8 +13,6 @@ export default function Header() {
     setIsMenuOpen(!isMenuOpen);
   };
 
-//   TODO: Update the layout & routes
-
   return (
     <header className="bg-primary text-primary-foreground shadow-md">
       <div className="container mx-auto px-4 py-4 flex items-center justify-between">
